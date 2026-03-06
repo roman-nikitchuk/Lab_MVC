ad746f22143b84e71c9e403ff5779cd46b512afd
NRM <roman.nikitchuk123@gmail.com>
2026-03-06 21:06:51 +0100

    Update Lab_2

--- Python/djangotutorial/mysite/urls.py
+++ Python/djangotutorial/mysite/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-import debug_toolbar
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
 ]
